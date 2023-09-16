3d192dcfa6d3955d45bbfd6a7d333afde1424693
Pericles Feitoza <periclesanfe@gmail.com>
2023-09-16 12:14:36 -0300

    Create mao.py

---
+++ mao.py
@@ -0,0 +1,18 @@
+from peca import peca
+
+class mao:
+    def __init__(self):
+        self.mao = []
+    
+    def __str__(self):
+        imagem = ''
+        for i in range(len(self.mao)):
+            imagem += str(self.mao[i]) + ' '
+        return imagem
+    
+    def add_peca(self, peca):
+        self.mao.append(peca)
+    
+    def tira_peca(self, peca):
+        return self.mao.pop(peca)
+    
