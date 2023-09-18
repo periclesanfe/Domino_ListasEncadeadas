4ea291c82f6a325ec642fc4097a5d00cb0b72425
Pericles Feitoza <periclesanfe@gmail.com>
2023-09-18 15:07:45 -0300

    refazer

--- main.py
+++ main.py
@@ -1,56 +1,172 @@
-# aqui vai ficar a vericação dos estados de jogo
-# as chamadas das funções
-# e a chamada da classe jogo
-from mao import mao_jogador
-from peca import peca
-import funcoes as func
-from tabuleiro import tabuleiro
+import random
 import os
 
-while True:
-    domino = func.criar_domino()
-    domino = func.embaralhar_domino(domino)
-    #func.mostrar_domino(domino)
-    QNTD_JOGADORES = int(input('Quantos jogadores: '))
-    while QNTD_JOGADORES < 2 or QNTD_JOGADORES > 4:
-        os.system('clear')
-        print('Número de jogadores inválido')
-        QNTD_JOGADORES = int(input('Informe a quantidade de jogadores: '))
-    jogadores = []
-    for i in range(QNTD_JOGADORES):
-        jogadores.append(func.criar_mao(domino, input(f'Informe o nome do jogador {i+1}: ')))
-    mesa = tabuleiro()
-    for vez, jogador in enumerate(jogadores):
-        mao_teste = jogador.mao
-        vez = 0
-        if any(peca.valor in mao_teste for peca in domino):
-            print('Jogador {} começa'.format(vez))
-            break
-            
-    print('Jogador {} começa'.format(vez+1))
-    while True:
-        print('Jogador {} sua vez'.format(vez+1))
-        print('Mesa: {}'.format(mesa))
-        print('Sua mão: {}'.format(jogadores[vez].mostrar_mao()))
-        peca_jogada = input(f'Qual peça deseja jogar? [0-{len(jogadores[vez].mao)-1}] ')
-        if peca_jogada.lower() == 'passar':
-            vez += 1
-            if vez == QNTD_JOGADORES:
-                vez = 0
-            continue
-        else:
-            peca_jogada = peca(peca_jogada)
-            if mesa.adiciona_peca(peca_jogada):
-               # mesa.adiciona_peca(peca_jogada)
-                jogadores[vez].tira_peca(peca_jogada)
-                if func.verifica_venceu(jogadores[vez]):
-                    print('Jogador {} venceu'.format(vez+1))
+# Classe de exceção para valores inválidos
+class ErroValorInvalido(Exception):
+    pass
+
+# Classe das peças
+class Peca:
+    def __init__(self, esquerda, direita):
+        self.valor = [esquerda, direita]
+        self.esquerda = esquerda # valores de esquerda e direita
+        self.direita = direita
+        # Estrutura para encadeamento de peças
+        self.anterior = None
+        self.proximo = None
+        
+    def __str__(self):
+        return f'[{self.esquerda}-{self.direita}]'
+    
+    def inverte_peca(self):
+        self.esquerda, self.direita = self.direita, self.esquerda
+        self.valor = [self.esquerda, self.direita]
+       
+# Classe que representa cada jogador 
+class Jogador:
+    def __init__(self, nome):
+        self.nome = nome
+        self.mao = []
+
+    def receber_peca(self, peca):
+        self.mao.append(peca)
+        
+    def retira_peca(self, index):
+        self.mao.pop(index)
+    
+    def mostra_mao(self):
+        saida = ''
+        for peca in self.mao:
+            saida += str(peca) + ' '
+        print(f"{self.nome}: {saida}")
+
+# Classe que representa o tabuleiro
+class Tabuleiro:
+    def __init__(self):
+        self.peças = []  # Lista de peças no tabuleiro
+        self.cabeça = None  # Valor da direita da última peça
+        self.calda = None  # Valor da esquerda da primeira peça
+
+    def adicionar_peca(self, peca, index):
+        # Verifica se a peça pode ser adicionada às extremidades
+        if not self.peças: # Se o tabuleiro estiver vazio, a primeira peça pode ser adicionada
+            self.peças.append(peca)
+            self.cabeça = peca.esquerda
+            self.calda = peca.direita
+            return True
+        elif self.cabeça in peca.valor:
+            if self.cabeça == peca.esquerda:
+                peca.inverte_peca()
+            peca.proximo = self.peças[0]
+            self.peças[0].anterior = peca
+            self.peças.insert(0, peca)
+            self.cabeça = peca.esquerda
+            jogador.retira_peca(index)
+            return True
+        elif self.calda in peca.valor:
+            if self.calda == peca.direita:
+                peca.inverte_peca()
+            peca.anterior = self.peças[-1]
+            self.peças[-1].proximo = peca
+            self.peças.append(peca)
+            self.calda = peca.direita
+            jogador.retira_peca(index)
+            return True
+        return False
+        
+        
+    def __str__(self):
+        if not self.peças:
+            return 'Tabuleiro vazio'
+        saida = ''
+        for peca in self.peças:
+            saida += str(peca) + ' '
+        return saida
+
+    def verificar_estado_do_jogo(self, jogador, qntd_passes):
+        if jogador.mao == []:
+            return 0
+        elif qntd_passes == 2:
+            return 1
+        return 2
+
+# Função para criar o conjunto de peças
+def criar_conjunto():
+    conjunto = []
+    for esquerda in range(7):
+        for direita in range(esquerda, 7):
+            peca = Peca(esquerda, direita)
+            conjunto.append(peca)
+    random.shuffle(conjunto) # Embaralha as peças
+    return conjunto
+
+# Cria as mãos dos jogadores
+def distribuir_maos(jogadores, domino_embaralhado):
+    for jogador in jogadores:
+        for i in range(7):
+            jogador.receber_peca(domino_embaralhado.pop())
+
+# mostar domino
+def mostrar_domino(domino):
+    saida = ''
+    for peca in domino:
+        saida += str(peca) + ' '
+    return saida
+
+while True: # Definir quantidade de jogadores
+    num_jogadores = int(input('Digite o número de jogadores: '))
+    if 2 <= num_jogadores <= 4:
+        break
+    os.system('clear')
+    print('O número de jogadores deve ser entre 2 e 4')
+    continue
+
+jogadores = []
+for i in range(num_jogadores):
+    nome = input(f'Digite o nome do jogador {i+1}: ')
+    jogador = Jogador(nome)
+    jogadores.append(jogador)
+
+domino = criar_conjunto()
+mostrar_domino(domino)
+distribuir_maos(jogadores, domino)
+tabuleiro = Tabuleiro()
+os.system('cls')
+
+qntd_passes = 0
+Estado_do_jogo = 2
+while Estado_do_jogo == 2:
+    for i in range(0, len(jogadores)):
+        jogador_vez = jogadores[i]
+        print(f'Vez do jogador {jogador_vez.nome}')
+        print(f'Tabuleiro: {tabuleiro}')
+        jogador_vez.mostra_mao()
+        while True:
+            try:
+                peca_escolhida = int(input('Digite o número da peça que deseja jogar: '))
+                if peca_escolhida == 0:
+                    qntd_passes += 1
+                    os.system('cls')
                     break
-                vez += 1
-                if vez == QNTD_JOGADORES:
-                    vez = 0
-                continue
-            else:
-                print('Peça inválida')
+                elif 1 <= peca_escolhida <= len(jogador_vez.mao):
+                    if tabuleiro.adicionar_peca(jogador_vez.mao[peca_escolhida-1], peca_escolhida-1):
+                        os.system('cls')
+                    break
+                else:
+                    raise ErroValorInvalido
+            except ErroValorInvalido:
+                print('Valor inválido')
                 continue
             
+        Estado_do_jogo = tabuleiro.verificar_estado_do_jogo(jogador_vez, qntd_passes)
+        if Estado_do_jogo == 0:
+            print(f'O jogador {jogador_vez.nome} venceu!')
+            break
+        elif Estado_do_jogo == 1:
+            print('O jogo empatou!')
+            break
+        elif Estado_do_jogo == 2:
+            continue
+        
+        if i == len(jogadores) - 1:
+            i = -1
